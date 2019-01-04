1bc64cf88988cf69279edf8dfda382077e1a9757
Marek Glowacki <m.glowacki24h@gmail.com>
2019-01-04 10:57:20 +0100

    Objects Human klass added.

---
+++ Zadania/Objects_and_packages/Ex1/Human.java
@@ -0,0 +1,30 @@
+package Objects_and_packages.Ex1;
+
+public class Human {
+
+    private int age;
+    private double weight;
+    private double height;
+    private String name;
+    private boolean male;
+
+    public int getAge(){
+        return age;
+    }
+
+    public double getWeight(){
+        return weight;
+    }
+
+    public double getHeight(){
+        return height;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public boolean isMale(){
+        return male;
+    }
+}
