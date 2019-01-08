7b0c816ced7a5b2d45c1b501be9754eb2a0ffcd8
Marek Glowacki <m.glowacki24h@gmail.com>
2019-01-08 10:10:53 +0100

    ex with Lopps and arrays cd

--- Zadania/Samouczek_programisty/Loops_n_if/ArrayFactory.java
+++ Zadania/Samouczek_programisty/Loops_n_if/ArrayFactory.java
@@ -1,12 +1,21 @@
 package Samouczek_programisty.Loops_n_if;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class ArrayFactory {
 
     private int x;
 
     public static void main(String[] args) {
+
         ArrayFactory arr = new ArrayFactory(1897);
-        arr.writeInt();
+//        arr.writeInt();
+//        System.out.println(arr.revString("Marek"));
+//        System.out.println(arr.palindrom("cos"));
+//        System.out.println(arr.palindrom("kajak"));
+//        System.out.println(arr.palindrom("kajaK"));
+        System.out.println(Arrays.deepToString(arr.matrix(4)));
     }
 
     public ArrayFactory(int x){
@@ -29,5 +38,37 @@ public class ArrayFactory {
         }
     }
 
+    public String revString(String word){
+        String rev = "";
+        for (int i=word.length(); i>0; i--){
+            rev += word.charAt(i-1);
+        } return rev;
+    }
 
+    public boolean palindrom(String word){
+        String rev = "";
+        for (int i=word.length(); i>0; i--){
+            rev += word.charAt(i-1);
+        }
+
+        if (rev.toLowerCase().equals(word.toLowerCase())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int[][] matrix(int number){
+        int[][] unitArr = new int[number][number];
+        for (int i=0; i<number; i++){
+            for(int j=0; j<number; j++){
+                if (i == j){
+                    unitArr[i][j] = 1;
+                } else {
+                    unitArr[i][j] = 0;
+                }
+            }
+        }
+        return unitArr;
+    }
 }
